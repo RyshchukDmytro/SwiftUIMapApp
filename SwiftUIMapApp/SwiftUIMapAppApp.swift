316fedf9f567808772fdb9eb170d2ba536ff617f
Dmytro <Dmytro.Ryshchuk@icloud.com>
2025-01-11 19:59:10 -0600

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Dmytro Ryshchuk on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
